82464016aea21771195270581d8f81348b7bd577
DEMYSTIF <mobinm.mohanan@gmail.com>
2023-09-10 21:53:16 +0530

    👔 | configured ApolloServer

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.log("Hello via Bun!");
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+
+// types
+import { typeDefs } from "./schema.ts";
+
+// resolvers
+import { resolvers } from "./resolvers.ts";
+
+// server setup
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+const { url } = await startStandaloneServer(server, {
+  listen: { port: 4000 },
+});
+
+console.log(`🚀  Server ready at: ${url}`);
